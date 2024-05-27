67fdba8845261afcb6d505bf479e2b0bddcf0aae
Max Burrows <maxajburrows@gmail.com>
2024-05-27 11:36:41 +0200

    Fetch account data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch("http://localhost:8080/accounts");
+      if (!response.ok) {
+        throw Error("Can't get accounts")
+      }
+      setData(await response.json());
+    }
+    fetchData();
+  }, []);
+
+
+
   return (
     <div className="App">
-      Hello World
+      {data.map((account) =>
+        <>
+          <p>{account.name}</p>
+          <p>{account.iban}</p>
+        </>)}
     </div>
   );
 }
