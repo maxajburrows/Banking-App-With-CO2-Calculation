a5e2b0e1c23d2f6cd6cf3d2488d6a4a54eec97d5
Max Burrows <maxajburrows@gmail.com>
2024-10-21 16:09:32 +0200

    Start jwt filter and make jwt service

---
+++ backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package nl.rabobank.banking_app.configuration;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String username;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        username = jwtService.extractUsername(jwtToken);
+    }
+}
---
+++ backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtService.java
@@ -0,0 +1,10 @@
+package nl.rabobank.banking_app.configuration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
