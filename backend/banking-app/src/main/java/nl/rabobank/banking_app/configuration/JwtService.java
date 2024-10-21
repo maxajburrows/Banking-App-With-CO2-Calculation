ebc540e8acc806d2bf8cf01e6639ce3e18946dbb
Max Burrows <maxajburrows@gmail.com>
2024-10-21 17:24:29 +0200

    Start extractAllClaims method

--- backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtService.java
+++ backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtService.java
@@ -2,9 +2,22 @@ package nl.rabobank.banking_app.configuration;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Service
 public class JwtService {
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
