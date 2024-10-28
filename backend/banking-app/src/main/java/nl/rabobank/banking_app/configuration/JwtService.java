a8de5cf7c051df83fbf6000d76e36cadad3c930f
Max Burrows <maxajburrows@gmail.com>
2024-10-28 13:49:26 +0100

    Write generate token method

--- backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtService.java
+++ backend/banking-app/src/main/java/nl/rabobank/banking_app/configuration/JwtService.java
@@ -1,23 +1,58 @@
 package nl.rabobank.banking_app.configuration;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
+    @Value("${jwt-secret-key}")
+    private String secretKey;
+
     public String extractUsername(String token) {
-        return null;
+        return extarctClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extarctClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 24)))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
+
     }
 
     private Claims extractAllClaims(String token) {
         return Jwts
-            .parserBuilder()
+            .parser()
             .setSigningKey(getSignInKey())
             .build()
             .parseClaimsJws(token)
             .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
