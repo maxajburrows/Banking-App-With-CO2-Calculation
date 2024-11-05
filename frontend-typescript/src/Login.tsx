b0321dec4e2c950a4f2c2b94c123b18b065d88c5
Max Burrows <maxajburrows@gmail.com>
2024-11-05 17:34:34 +0100

    Fully adapt new login page

--- frontend-typescript/src/Login.tsx
+++ frontend-typescript/src/Login.tsx
@@ -26,37 +26,9 @@ function Login() {
         }
     }
 
-    // return (<>
-    //     {failedLogin && <p>Incorrect username or password</p>}
-    //     <div className="container vh-100">
-    //         <div className="row justify-content-center align-content-center vh-100">
-    //             <div className="col-md-7 text-center">
-    //                 <div className="text-center">
-    //                     <h1>Basic Bank</h1>
-    //                     <p className="col-8 mx-auto">No thrills banking. We keep your money safe. That's it.</p>
-    //                     <h3>Login</h3>
-    //                     <form onSubmit={handleLogin}>
-    //                         <div className="form-group">
-    //                             <label className="col" htmlFor="username">Username</label>
-    //                             <input type="text" id="username" className="form-control" placeholder="Enter username" value={username}
-    //                                    onChange={e => setUsername(e.target.value)}/>
-    //                         </div>
-    //                         <div className="form-group">
-    //                             <label className="text-" htmlFor="password">Password</label>
-    //                             <input type="password" id="password" className="form-control" placeholder="Enter password" value={password}
-    //                                    onChange={e => setPassword(e.target.value)}/>
-    //                         </div>
-    //                         <button type="submit" className="btn btn-primary">Login</button>
-    //                     </form>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </>);
-
     return (<>
 
-    <section className="background-radial-gradient overflow-hidden">
+    <section className="background-radial-gradient overflow-hidden vh-100 justify-content-center">
         <div className="container px-4 px-md-5 text-center text-lg-start my-5">
             <div className="row gx-lg-5 align-items-center mb-5">
                 <div className="col-lg-6 mb-5 mb-lg-0" style={{zIndex: 10}}>
@@ -75,61 +47,23 @@ function Login() {
 
                     <div className="card bg-glass">
                         <div className="card-body px-4 py-5 px-md-5">
-                            <form>
-                                <div className="row">
-                                    <div className="col-md-6 mb-4">
-                                        <div data-mdb-input-init className="form-outline">
-                                            <input type="text" id="form3Example1" className="form-control" />
-                                            <label className="form-label" htmlFor="form3Example1">First name</label>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6 mb-4">
-                                        <div data-mdb-input-init className="form-outline">
-                                            <input type="text" id="form3Example2" className="form-control" />
-                                            <label className="form-label" htmlFor="form3Example2">Last name</label>
-                                        </div>
-                                    </div>
-                                </div>
+                            <form onSubmit={handleLogin}>
+                                {failedLogin && <p>Incorrect username or password</p>}
 
                                 <div data-mdb-input-init className="form-outline mb-4">
-                                    <input type="email" id="form3Example3" className="form-control" />
                                     <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                    <input type="email" id="form3Example3" className="form-control" placeholder="Enter your username" value={username}
+                                           onChange={e => setUsername(e.target.value)}/>
                                 </div>
 
                                 <div data-mdb-input-init className="form-outline mb-4">
-                                    <input type="password" id="form3Example4" className="form-control" />
-                                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                                <label className="form-label" htmlFor="form3Example4">Password</label>
+                                    <input type="password" id="form3Example4" className="form-control" placeholder="Enter your password" value={password}
+                                           onChange={e => setPassword(e.target.value)}/>
                                 </div>
-
-                                <div className="form-check d-flex justify-content-center mb-4">
-                                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                                    <label className="form-check-label" htmlFor="form2Example33">
-                                        Subscribe to our newsletter
-                                    </label>
-                                </div>
-
-                                <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block mb-4">
-                                    Sign up
+                                <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block">
+                                    Login
                                 </button>
-
-                                <div className="text-center">
-                                    <p>or sign up with:</p>
-                                    <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-link btn-floating mx-1">
-                                        <i className="fab fa-facebook-f"></i>
-                                    </button>
-
-                                    <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-link btn-floating mx-1">
-                                        <i className="fab fa-google"></i>
-                                    </button>
-
-                                    <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-link btn-floating mx-1">
-                                        <i className="fab fa-twitter"></i>
-                                    </button>
-
-                                    <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-link btn-floating mx-1">
-                                        <i className="fab fa-github"></i>
-                                    </button>
-                                </div>
                             </form>
                         </div>
                     </div>
