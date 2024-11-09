bd5a8a7fb4157da972c38089ef796206b1630b89
Max Burrows <maxajburrows@gmail.com>
2024-11-09 16:39:53 +0100

    Mostly finish styling homepage

--- frontend-typescript/src/components/Homepage.tsx
+++ frontend-typescript/src/components/Homepage.tsx
@@ -38,38 +38,43 @@ function Homepage() {
 
     return (
         <>
-        <section className="background-radial-gradient overflow-hidden vh-100 justify-content-center">
-            <div className="container px-4 px-md-5 text-center text-lg-start my-5">
-                <h1 className="my-5 display-5 fw-bold ls-tight" style={{color: 'hsl(218, 81%, 95%)'}}>Welcome back {usersName}</h1>
-                <div className="row gx-lg-5 align-items-center mb-5">
-                        <button className="col btn btn-outline-light" style={{height: '100%'}} onClick={() => navigate(`/accounts/${username}/transfer`)}>
-                            <h2>Transfer money</h2>
-                        </button>
+            <section className="background-radial-gradient overflow-hidden vh-100 justify-content-center">
+                <div className="container px-4 px-md-5 text-center text-lg-start my-5">
+                    <div className="row gx-lg-5 align-items-center mb-5">
+                        <h1 className="my-5 display-5 fw-bold ls-tight" style={{color: 'hsl(218, 81%, 95%)'}}>Welcome back {usersName}</h1>
+                        <div className="row gx-lg-5 align-items-center mb-5">
+                            <button className="col btn btn-outline-light" style={{height: '100%'}} onClick={() => navigate(`/accounts/${username}/transfer`)}>
+                                <h2>Transfer money</h2>
+                            </button>
+                            <button className="col btn btn-outline-light" style={{height: '100%'}} onClick={() => navigate(`/accounts/${accounts[1].iban}/insights`)}>
+                                <h2>Insights</h2>
+                            </button>
+                        </div>
+                    </div>
 
-                        <button className="col btn btn-outline-light" style={{height: '100%'}} onClick={() => navigate(`/accounts/${accounts[1].iban}/insights`)}>
-                            <h2>Insights</h2>
-                        </button>
+                    <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+                        <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+                        <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
 
+                        <div className="card blur-bg">
+                            <div className="card-body blur-bg px-4 py-5 px-md-5">
+                                <h2 style={{color: 'hsl(218, 81%, 95%)'}}>Your Accounts</h2>
+                                {accounts.map((account) => (
+                                            <button type="button" className="btn btn-outline-light account-button" onClick={() => goToAccount(account.iban)}>
+                                                <h2>{account.accountName}</h2>
+                                                <p>{account.iban}</p>
+                                                <p>€{account.balance.toFixed(2)}</p>
+                                            </button>
+                                ))}
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-                    <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-                    <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
-
-                    <h1>Your Accounts</h1>
-                    {accounts.map((account) => (
-                        <button type="button" className="btn btn-outline-dark btn-lg btn-block" onClick={() => goToAccount(account.iban)}>
-                            <h2>{account.accountName}</h2>
-                            <p>{account.iban}</p>
-                            <p>€{account.balance.toFixed(2)}</p>
-                        </button>
-                    ))}
-                </div>
-            </div>
-
-        </section>
-</>
-)
-    ;
+            </section>
+        </>
+    )
+        ;
 }
 
 export default Homepage;
